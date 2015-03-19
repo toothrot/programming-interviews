f8d85082ff4ea01455c01aef7a90242be91a0093
Alex Rakoczy <scissorjammer@gmail.com>
2015-03-18 21:49:57 -0400

    Play with godoc

--- programming-interviews/go/stack/stack.go
+++ programming-interviews/go/stack/stack.go
@@ -1,20 +1,22 @@
 package stack
 
+// The Element knows its value and what the next element is
 type Element struct {
 	next  *Element
 	Value interface{}
 }
 
+// The Stack structure only contains a head element
 type Stack struct {
 	head *Element
 }
 
+// Creates a new stack
 func New() *Stack { return new(Stack) }
 
+// Returns the number of items of the stack
 func (s *Stack) Len() int {
-	if s.head == nil {
-		return 0
-	} else {
+	if s.head != nil {
 		count := 0
 		n := s.head
 
@@ -29,12 +31,15 @@ func (s *Stack) Len() int {
 
 		return count
 	}
+	return 0
 }
 
+// BUG Not yet implemented
 func (s *Stack) Head() *Element {
 	return nil
 }
 
+// Pushes a new value onto the stack
 func (s *Stack) Push(v interface{}) *Element {
 	e := new(Element)
 	e.Value = v
@@ -43,6 +48,7 @@ func (s *Stack) Push(v interface{}) *Element {
 	return e
 }
 
+// Pops the top value from the stack
 func (s *Stack) Pop() interface{} {
 	e := s.head
 	if e != nil {
